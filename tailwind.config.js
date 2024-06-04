33c3ad8920b5f416bd0b6a64418daeacffd9a4a9
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2024-06-04 12:48:38 -0300

    Co-authored-by: Carlos Santos
    add flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}","./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       colors: {
         'mobile-1': '#999999',
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }}
