67dcf410e19037b96e12b02b70b2121b64fb44f9
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2024-06-07 21:48:58 -0300

    forms zap e mudança de bg do botões

---
+++ src/form.js
@@ -0,0 +1,12 @@
+function sendwhatsapp(){
+    var telefone = "83993930599";
+
+    var nome = document.getElementById("first_name").value;
+    var sobrenome = document.getElementById("last_name").value;
+    var idade = document.getElementById("age").value;
+    var descricao = document.getElementById("description").value;
+
+    var mensagem = "Olá, meu nome é " + "*" +nome + " " + sobrenome + "*" + ", tenho " +"*"+ idade + "*"+ " anos e entro em contato com vocês, devido que " + descricao;
+
+    window.open("https://api.whatsapp.com/send?phone=55" + telefone + "&text=" + mensagem, "_blank").focus();
+}
