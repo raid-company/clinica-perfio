2f2dd163c4e6dfa8d29c2d23496a42da56b2afa2
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2024-07-08 13:34:23 -0300

    update

--- src/form.js
+++ src/form.js
@@ -1,12 +1,16 @@
-function sendwhatsapp(){
-    var telefone = "83993930599";
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do formulário
+    sendwhatsapp(); // Chama a função para enviar a mensagem no WhatsApp
+  });
 
+  function sendwhatsapp() {
+    var telefone = "83993930599";
     var nome = document.getElementById("first_name").value;
     var sobrenome = document.getElementById("last_name").value;
     var idade = document.getElementById("age").value;
     var descricao = document.getElementById("description").value;
 
-    var mensagem = "Olá, meu nome é " + "*" +nome + " " + sobrenome + "*" + ", tenho " +"*"+ idade + "*"+ " anos e entro em contato com vocês, devido que " + descricao;
+    var mensagem = "Olá, meu nome é *" + nome + " " + sobrenome + "*, tenho *" + idade + "* anos e entro em contato com vocês, devido que " + descricao;
 
-    window.open("https://api.whatsapp.com/send?phone=55" + telefone + "&text=" + mensagem, "_blank").focus();
-}
+    window.open("https://api.whatsapp.com/send?phone=55" + telefone + "&text=" + encodeURIComponent(mensagem), "_blank").focus();
+  }
